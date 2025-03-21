841854338b9d408ccb733368101f3474a539c41b
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-21 08:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let time = document.getElementById("timer");
+
+let currentDate = new Date();
+
+setInterval(
+	function(){
+		 currentDate = new Date();
+		time.innterHtml= currentDate.toLocaleString();
+	},
+	1000
+)
